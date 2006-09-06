5bfd7b7f04c7acadf34994ee1b57a0502d27478b
Chad Woolley <thewoolleyman@gmail.com>
2006-09-06 05:12:40 +0000

    add logic in config to support default install_options
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@32 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/config.rb
+++ lib/geminstaller/config.rb
@@ -8,16 +8,31 @@ module GemInstaller
     end
 
     def gems
+      parse_defaults
       gem_defs = @yaml["gems"]
       gems = []
       gem_defs.each do |gem_def|
         name = gem_def['name']
         version = gem_def['version'].to_s
+        # get install_options for specific gem, if specified
         install_options = gem_def['install_options'].to_s
+        # if no install_options were specified for specific gem, and default install_options were specified...
+        if install_options.empty? && @default_install_options then
+          # then use the default install_options
+          install_options = @default_install_options
+        end
         gem = GemInstaller::Gem.new(name, version, install_options)
         gems << gem
       end
       return gems
+    end
+
+    protected
+
+    def parse_defaults
+      defaults = @yaml["defaults"]
+      return if defaults.nil?
+      @default_install_options = defaults['install_options']
     end
   end
 end
--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -49,3 +49,44 @@ context "YAML containing two gems with the same name but different versions" do
     gem.install_options.should_equal('-y')
   end
 end
+
+context "YAML containing default install_options" do
+  setup do
+    @yaml_text = <<-STRING_END
+      defaults:
+          install_options: -y
+      gems:
+        - name: mygem
+          version: 1.1
+    STRING_END
+    @yaml = YAML.load(@yaml_text)
+    @config = GemInstaller::Config.new(@yaml)
+  end
+
+  specify "should propogate default install_options into gem object" do
+    gem = @config.gems[0]
+    gem.name.should_equal('mygem')
+    gem.version.should_equal('1.1')
+    gem.install_options.should_equal('-y')
+  end
+end
+
+context "YAML containing neither default install_options nor gem-specific install options" do
+  setup do
+    @yaml_text = <<-STRING_END
+      gems:
+        - name: mygem
+          version: 1.1
+    STRING_END
+    @yaml = YAML.load(@yaml_text)
+    @config = GemInstaller::Config.new(@yaml)
+  end
+
+  specify "should have no install_options set on gem object" do
+    gem = @config.gems[0]
+    gem.name.should_equal('mygem')
+    gem.version.should_equal('1.1')
+    gem.install_options.should_equal('')
+  end
+end
+
