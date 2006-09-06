00b028ff7fe7b96d4d0abff4322e977b53adeb3d
Chad Woolley <thewoolleyman@gmail.com>
2006-09-06 04:46:27 +0000

    add install_options attribute to gem class
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@28 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/gem.rb
+++ lib/geminstaller/gem.rb
@@ -2,9 +2,10 @@ require 'yaml'
 
 module GemInstaller
   class Gem
-    def initialize(name, version)
+    def initialize(name, version, install_options)
       @name = name
       @version = version
+      @install_options = install_options
     end
 
     def name
@@ -13,6 +14,10 @@ module GemInstaller
 
     def version
       @version
+    end
+
+    def install_options
+      @install_options
     end
   end
 end
--- spec/unit/gem_spec.rb
+++ spec/unit/gem_spec.rb
@@ -4,11 +4,12 @@ require File.expand_path("#{dir}/../../lib/geminstaller/gem")
 
 context "A gem" do
   setup do
-    @gem = GemInstaller::Gem.new('mygem', 'v1.1')
+    @gem = GemInstaller::Gem.new('mygem', 'v1.1', '-y')
   end
 
-  specify "should contain a name and a version" do
+  specify "should contain a name, a version, and install options" do
     @gem.name.should_equal('mygem')
     @gem.version.should_equal('v1.1')
+    @gem.install_options.should_equal('-y')
   end
 end
