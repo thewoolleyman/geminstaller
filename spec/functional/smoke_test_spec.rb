d31d5906609f8a773a1e8159f88ab31be1f987ff
Chad Woolley <thewoolleyman@gmail.com>
2006-10-19 06:25:14 +0000

    clean up dependency injector test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@57 8563d94c-3d18-0410-b980-ae029421534c

--- spec/functional/smoke_test_spec.rb
+++ spec/functional/smoke_test_spec.rb
@@ -2,15 +2,15 @@ dir = File.dirname(__FILE__)
 require File.expand_path("#{dir}/../spec_helper")
 require File.expand_path("#{dir}/../../lib/geminstaller/dependency_injector")
 
-context "Placeholder for real functional tests" do
+context "Dependency Injector" do
   setup do
   end
 
-  specify "should placehold" do
+  specify "should successfully assemble an application object" do
     dependency_injector = GemInstaller::DependencyInjector.new
     test_config_file_path = File.expand_path("#{dir}/test_geminstaller_config.yml")
     dependency_injector.config_file_path = test_config_file_path
     application = dependency_injector.registry.app
-    # application.run
+    application.should_not_be_nil
   end
 end
