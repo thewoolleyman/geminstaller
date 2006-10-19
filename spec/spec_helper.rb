f1c23fa1428219cc493c8dbf041e09e6e7b455eb
Chad Woolley <thewoolleyman@gmail.com>
2006-10-19 06:45:04 +0000

    clean up test suite, remove unnecessary stuff
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@60 8563d94c-3d18-0410-b980-ae029421534c

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,14 +10,10 @@ args << "--diff"
 args << $0
 $context_runner  = ::Spec::Runner::OptionParser.create_context_runner(args, false, STDERR, STDOUT)
 
-def run_context_runner_if_necessary(system_exit, has_run)
-  p system_exit unless system_exit.nil?
-  return if system_exit && !system_exit.success?
-  return if has_run
+def run_context_runner
   exit context_runner.run(true)
 end
 
 at_exit do
-  has_run = !context_runner.instance_eval {@reporter}.instance_eval {@start_time}.nil?
-  run_context_runner_if_necessary($!, has_run)
+  run_context_runner
 end
--- test/test_suite.rb
+++ test/test_suite.rb
@@ -4,23 +4,4 @@ dir = File.dirname(__FILE__)
 @@test_files = Dir.glob("#{dir}/../spec/**/*_spec.rb")
 @@test_files.each {|x| require File.expand_path(x)}
 
-class TestSuite
-  def self.suite
-    suite = Test::Unit::TestSuite.new
-
-    @@test_files.each do |filename|
-      File.open(filename) do |file|
-        file.each_line do |line|
-          if match = (/class\s+(\w+Test)/).match(line)
-           suite << eval(match[1] + ".suite")
-          end
-        end
-      end
-    end
-    return suite
-
-  end
-end
-
-result = Test::Unit::AutoRunner.run
-exit result
+exit 0
