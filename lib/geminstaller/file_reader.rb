eaf3fd2cb6123251cc7981017b9e6b316da63179
Chad Woolley <thewoolleyman@gmail.com>
2006-10-22 08:50:08 +0000

    update todo, finish refactor for error message handling if file io problems occur
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@69 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/file_reader.rb
+++ lib/geminstaller/file_reader.rb
@@ -3,23 +3,20 @@ module GemInstaller
     def read(file_path)
       file_contents = nil
       if !File.exist?(file_path) then
-        $stderr.print "Error: File #{file_path} does not exist.  Please ensure this file exists\n\n"
-        raise
+        raise RuntimeError.new("Error: File #{file_path} does not exist.  Please ensure this file exists\n")
       end
 
       file = nil
       begin
         file = File.open(file_path)
       rescue
-        $stderr.print "Error: Unable open file #{file_path}.  Please ensure this file can be opened.\n\n"
-        raise
+        raise RuntimeError.new("Error: Unable open file #{file_path}.  Please ensure this file can be opened.\n")
       end
 
       begin
         file.read
       rescue
-        $stderr.print "Error: Unable read file #{file_path}.  Please ensure this file can be read.\n\n"
-        raise
+        raise RuntimeError.new("Error: Unable read file #{file_path}.  Please ensure this file can be read.\n")
       end
     end
   end
--- spec/unit/file_reader_spec.rb
+++ spec/unit/file_reader_spec.rb
@@ -2,6 +2,7 @@ dir = File.dirname(__FILE__)
 require File.expand_path("#{dir}/../spec_helper")
 
 context "An existing file" do
+  # TODO: should test the exceptions and messages too...
   specify "should be openable" do
     testfile_path = File.expand_path("#{dir}/testfile.txt")
     file_reader = GemInstaller::FileReader.new
