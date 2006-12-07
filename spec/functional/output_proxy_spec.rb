aef99919d63871b1483dc1b4088530f998581f07
Chad Woolley <thewoolleyman@gmail.com>
2006-12-07 07:48:55 +0000

    add test for output_proxy_spec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@129 8563d94c-3d18-0410-b980-ae029421534c

---
+++ spec/functional/output_proxy_spec.rb
@@ -0,0 +1,50 @@
+dir = File.dirname(__FILE__)
+require File.expand_path("#{dir}/../spec_helper")
+
+class MockStderr
+  attr_reader :err
+  def write(out)
+  end
+  
+  def print(err)
+    @err = err
+  end
+end
+
+class MockStdout
+  attr_reader :out
+  def write(out)
+  end
+  
+  def print(out)
+    @out = out
+  end
+end
+
+context "The output proxy" do
+  include GemInstaller::SpecUtils
+  setup do
+    @output_proxy = GemInstaller::OutputProxy.new
+    @original_stdout = $stdout
+    @mock_stdout = MockStdout.new
+    $stdout = @mock_stdout
+    @original_stderr = $stderr
+    @mock_stderr = MockStderr.new
+    $stderr = @mock_stderr
+  end
+
+  specify "should proxy to stdout and stderr" do
+    sysout = "out!"
+    syserr = "err!"
+    @output_proxy.sysout(sysout)
+    @output_proxy.syserr(syserr)
+    @mock_stdout.out.should==(sysout)
+    @mock_stderr.err.should==(syserr)
+  end
+  
+  teardown do
+    $stdout = @original_stdout
+    $stderr = @original_stderr
+  end
+
+end
