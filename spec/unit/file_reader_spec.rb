6c0f36c278ab88a55612fc648edb91a0cdb08617
Chad Woolley <thewoolleyman@gmail.com>
2006-12-07 06:04:34 +0000

    test error handling
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@124 8563d94c-3d18-0410-b980-ae029421534c

--- spec/unit/file_reader_spec.rb
+++ spec/unit/file_reader_spec.rb
@@ -2,11 +2,18 @@ dir = File.dirname(__FILE__)
 require File.expand_path("#{dir}/../spec_helper")
 
 context "An existing file" do
-  # TODO: should test the exceptions and messages too...
   specify "should be openable" do
     testfile_path = File.expand_path("#{dir}/testfile.txt")
     file_reader = GemInstaller::FileReader.new
     file_contents = file_reader.read(testfile_path)
     file_contents.should==('test')
+  end
+end
+
+context "A missing file" do
+  specify "should return an error message" do
+    testfile_path = File.expand_path("missing_file.txt")
+    file_reader = GemInstaller::FileReader.new
+    lambda { file_reader.read(testfile_path) }.should_raise GemInstaller::GemInstallerError
   end
 end
