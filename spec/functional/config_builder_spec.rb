e119450bdbb5472eee2f75f10c843237a6f63c4c
Chad Woolley <thewoolleyman@gmail.com>
2006-12-22 00:11:54 +0000

    clean up tests to verify multiple config file support
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@188 8563d94c-3d18-0410-b980-ae029421534c

--- spec/functional/config_builder_spec.rb
+++ spec/functional/config_builder_spec.rb
@@ -12,40 +12,43 @@ context "a config builder with a single config file path" do
     config_builder = dependency_injector.registry.config_builder
     config = config_builder.build_config
     config.gems[0].name.should==("testgem1")
+    config.gems[1].check_for_upgrade.should==(true)
   end
 end
 
 context "a config builder with multiple config file paths" do
   setup do
-  end
-  
-  specify "should successfully assemble a config object" do
     test_config_file_paths = 
       File.expand_path("#{dir}/test_geminstaller_config.yml") + "," + 
       File.expand_path("#{dir}/test_geminstaller_config_2.yml")
     dependency_injector = GemInstaller::DependencyInjector.new
     dependency_injector.config_file_paths = test_config_file_paths
     config_builder = dependency_injector.registry.config_builder
-    config = config_builder.build_config
+    @config = config_builder.build_config
+  end
+  
+  specify "should successfully assemble a config object" do
+    @config.gems[0].name.should==("testgem1")
+    @config.gems[0].check_for_upgrade.should==(false)
     
-    config.gems[0].name.should==("testgem1")
-    
-    config.gems[1].name.should==("testgem1")
-    config.gems[1].version.should==("v2.0")
-    
-    config.gems[2].name.should==("testgem2")
-    config.gems[2].install_options.should==(["--backtrace"])
-    
-    config.gems[3].name.should==("testgem3")
+    @config.gems[1].name.should==("testgem1")
+    @config.gems[1].version.should==("v2.0")
   end
 
   specify "should allow subsequent files to override defaults" do
+    @config.gems[2].name.should==("testgem2")
+    @config.gems[2].check_for_upgrade.should==(false)
   end
   
   specify "should allow subsequent files to override a gem"do
+    @config.gems[2].name.should==("testgem2")
+    @config.gems[2].install_options.should==(["--backtrace"])
   end
 
   specify "should allow subsequent files to specify new gems"do
+    @config.gems[3].name.should==("testgem3")
+    @config.gems[3].version.should==("v3.0")
+    @config.gems[3].check_for_upgrade.should==(true)
   end
 
 end
