08dcaba3ff8229a10ec8d0701d93f09b99864cc9
Chad Woolley <thewoolleyman@gmail.com>
2006-12-24 11:11:08 +0000

    remove code that is unnecessary since we don't use test_unit
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@203 8563d94c-3d18-0410-b980-ae029421534c

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,25 +1,15 @@
-require 'test/unit'
-
 dir = File.dirname(__FILE__)
-#@@test_files = Dir.glob("#{dir}/../spec/**/*_spec.rb")
+
+require 'test/unit'
+require File.expand_path("#{dir}/../spec/spec_helper")
+
 @@test_files = Dir.glob("#{dir}/../spec/**/*_spec.rb")
 @@test_files.each {|x| require File.expand_path(x)}
 
 class TestSuite
   def self.suite
     suite = Test::Unit::TestSuite.new
-
-    @@test_files.each do |filename|
-      File.open(filename) do |file|
-        file.each_line do |line|
-          if match = (/class\s+(\w+Test)/).match(line)
-           suite << eval(match[1] + ".suite")
-          end
-        end
-      end
-    end
     return suite
-
   end
 end
 
