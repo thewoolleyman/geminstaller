fba6281e1162f284089abc8090de9c7f3966d8a5
Chad Woolley <thewoolleyman@gmail.com>
2006-12-28 05:11:55 +0000

    trying to get gem_command_line_proxy working on all platforms
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@225 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/gem_command_line_proxy.rb
+++ lib/geminstaller/gem_command_line_proxy.rb
@@ -5,7 +5,15 @@ module GemInstaller
       output = `gem #{args_string} 2>&1`
       return_value = $?
       lines = output.split("\n")
-      if (return_value != 0)
+      # return value is always zero on windows (from gem.bat), so we have to resort to parsing the output
+      error_in_output = false
+      lines.each do |line|
+        if line.match(/^ERROR.*/)
+          error_in_output = true
+          break
+        end
+      end
+      if (return_value != 0 or error_in_output)
         error_message = "Error: gem command failed.  Command was 'gem #{args_string}', return value was '#{return_value}'.  Output was:\n"
         lines.each do |line| 
           error_message += '  ' + line + '\n'
