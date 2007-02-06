386f727f02fee56d1b378df31cd91218e46bc501
Chad Woolley <thewoolleyman@gmail.com>
2007-02-06 20:43:26 +0000

    r297@chadmac:  woolley | 2007-02-03 21:00:29 -0700  make sure version requirements like ~> are handled correctly
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@269 8563d94c-3d18-0410-b980-ae029421534c

--- spec/functional/version_specifier_spec.rb
+++ spec/functional/version_specifier_spec.rb
@@ -19,8 +19,18 @@ context "a VersionSpecifier instance" do
     should_specify("> 1.2.3", '2.0, 1.2.4, 1.2.4, 0.3', '2.0')
   end
   
+  specify "should handle ~> version requirement operator (greater minor version ok, but not major)" do
+    should_specify("~> 1.2", '2.1, 2.0, 1.3, 1.2, 1.1', '1.3')
+  end
+
+  # TODO: Should this work?  Gem::Version::Requirement says it should...
+#  specify "should handle multiple version requirements" do    
+#    should_specify("> 0.3.13.1, < 0.3.13.4", '0.3.13.4, 0.3.13.3, 0.3.13.2, 0.3.13.1', '0.3.13.3')
+#  end
+
   specify "should throw an error if no matching versions are found" do    
     lambda { @version_specifier.specify("> 2", '2, 1') }.should_raise(GemInstaller::GemInstallerError)
+    lambda { @version_specifier.specify("!= 2", '2') }.should_raise(GemInstaller::GemInstallerError)
   end
   
   def should_specify(version_requirement, available_versions, expected_specified_version)
