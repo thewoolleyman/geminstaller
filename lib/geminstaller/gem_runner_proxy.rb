6bdc55ee98908c55c7117533605ac05d52aa0148
Chad Woolley <thewoolleyman@gmail.com>
2007-02-27 19:12:08 +0000

    make error output stand out more
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@334 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/gem_runner_proxy.rb
+++ lib/geminstaller/gem_runner_proxy.rb
@@ -57,7 +57,9 @@ module GemInstaller
     
     def raise_error_with_output(exit_error, message, listener, args)
       args_string = args.join(" ")
-      descriptive_exit_message = "\n#{message}\n"
+      descriptive_exit_message = "\n=========================================================\n"
+      descriptive_exit_message += "#{message}\n"
+      descriptive_exit_message += "=========================================================\n\n"
       descriptive_exit_message += "Gem command was:\n  gem #{args_string}\n\n"
       descriptive_exit_message += "Gem command output was:\n"
       descriptive_exit_message += listener.read!.join("\n")
