a69f1e4f7b12bb31676acfc5fffb56a84309446e
Chad Woolley <thewoolleyman@gmail.com>
2007-02-28 00:32:50 +0000

    test coverage for case where terminate_interaction is called with zero exit code
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@338 8563d94c-3d18-0410-b980-ae029421534c

--- spec/functional/gem_runner_proxy_spec.rb
+++ spec/functional/gem_runner_proxy_spec.rb
@@ -21,6 +21,17 @@ context "a GemRunnerProxy instance" do
     output.join("\n").should_match(expected_output)
   end
 
+  specify "should not throw an error if there is an normal rubygems exit via terminate_interaction" do
+    gem_runner_args = ["--help"]
+  
+    begin
+      @gem_runner_proxy.run(gem_runner_args)
+    rescue GemInstaller::GemInstallerError => error
+      expected_error_message = /Gem command was:.*--help.*Gem command output was:.*Usage:/m
+      error.message.should_match(expected_error_message)
+    end
+  end
+
   specify "should return error output if there is an abnormal exit" do
     gem_runner_args = ["bogus_command"]
 
