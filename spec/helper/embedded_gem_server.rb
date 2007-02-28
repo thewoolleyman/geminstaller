a42d35b31c68456d7b74a7a1feef3fba1d3aa995
Chad Woolley <thewoolleyman@gmail.com>
2007-02-28 20:22:41 +0000

    make windows tests pass via 'ruby test\test_all.rb, but not via 'rake test'
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@352 8563d94c-3d18-0410-b980-ae029421534c

--- spec/helper/embedded_gem_server.rb
+++ spec/helper/embedded_gem_server.rb
@@ -4,8 +4,15 @@ module GemInstaller
     def self.start
       return if @@gem_server_pid
       Gem.clear_paths
-      gem_server_process = IO.popen("gem_server --dir=#{embedded_gem_dir} --port=#{GemInstaller::SpecUtils.embedded_gem_server_port}")
-      @@gem_server_pid = gem_server_process.pid
+      cmd_args = "--dir=#{embedded_gem_dir} --port=#{GemInstaller::SpecUtils.embedded_gem_server_port}"
+      if windows?
+        io_handles_and_pid = Open4.popen4("gem_server.bat #{cmd_args}",'b',true)
+        pid = io_handles_and_pid[3]
+        @@gem_server_pid = pid
+      else
+        gem_server_process = IO.popen("gem_server #{cmd_args}")
+        @@gem_server_pid = gem_server_process.pid
+      end
       print "Started embedded gem server at #{embedded_gem_dir}, pid = #{@@gem_server_pid}\n"
       trap("INT") { Process.kill(9,@@gem_server_pid); exit! }
       # TODO: avoid sleep by detecting when gem_server port comes up
@@ -20,10 +27,29 @@ module GemInstaller
       stopped = false
       if @@gem_server_pid
         print "Killing embedded gem server at pid = #{@@gem_server_pid}\n"
-        Process.kill(9,@@gem_server_pid)
+        kill_result = Process.kill(1,@@gem_server_pid)
+        if windows?
+          kill_result.each do |pid|
+            print "  Killed pid: #{pid}\n"
+          end
+          print "NOTE: On windows, there is an orphaned gem_server ruby
+      process left, which was a grandchild of the top-level ruby
+      process which runs the tests, and a child of the cmd process
+      which runs gem_server.bat.  I can't figure out how to make
+      it die.  Luckily, there only seems to be one alive at any
+      given time, not an additional one for each test run, and this
+      one dies when the console used to run the tests is closed.\n\n"
+         print "WARNING: the windows tests are currently only passing
+      when run via 'rake test/test_all.rb.  They fail when run via
+      'rake test'\n"
+        end
         stopped = true
       end
       return stopped
+    end
+    
+    def self.windows?
+      RUBY_PLATFORM.index('mswin')
     end
   end
 end
--- spec/helper/spec_helper.rb
+++ spec/helper/spec_helper.rb
@@ -6,6 +6,10 @@ require 'stringio'
 require File.expand_path("#{dir}/spec_utils.rb")
 require File.expand_path("#{dir}/test_gem_home.rb")
 require File.expand_path("#{dir}/embedded_gem_server.rb")
+if RUBY_PLATFORM.index('mswin')
+  require 'win32/process'
+  require 'win32/open3'
+end
 
 args = ARGV.dup
 unless args.include?("-f") || args.include?("--format")
