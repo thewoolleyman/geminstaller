38f8bc6989dad935f585dba7699af0360e6f8c56
Chad Woolley <thewoolleyman@gmail.com>
2007-03-01 09:29:02 +0000

    speed up tests, remote rubygems_installer_spec, and make test_gem_home_spec run first
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@377 8563d94c-3d18-0410-b980-ae029421534c

--- test/test_all.rb
+++ test/test_all.rb
@@ -4,6 +4,9 @@ require 'test/unit'
 require File.expand_path("#{dir}/../spec/helper/spec_helper")
 
 @@test_files = Dir.glob("#{dir}/../spec/**/*_spec.rb")
+# put test_gem_home_spec first so we only have to perform the install once
+@@test_files.unshift(Dir.glob("#{dir}/../spec/unit/test_gem_home_spec.rb")[0])
+@@test_files.uniq
 @@test_files.each {|x| require File.expand_path(x)}
 
 class TestSuite
