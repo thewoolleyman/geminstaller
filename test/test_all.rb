bd8100da1d041924a0d3ec64c9c82e8ada1f08c2
Chad Woolley <thewoolleyman@gmail.com>
2007-03-03 06:46:40 +0000

    make test suite pass in gem, with message that specs are not included in the gem
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@393 8563d94c-3d18-0410-b980-ae029421534c

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,7 +1,12 @@
 dir = File.dirname(__FILE__)
 
 require 'test/unit'
-require File.expand_path("#{dir}/../spec/helper/spec_helper")
+begin
+  require File.expand_path("#{dir}/../spec/helper/spec_helper")
+rescue LoadError
+  print "\nUnable to run specs/tests.  They are not included in the gem, in order to keep the gem size down.  If you want to run them, check out the geminstaller source.\n\n"
+  exit 0
+end
 
 @@test_files = Dir.glob("#{dir}/../spec/**/*_spec.rb")
 # put test_gem_home_spec first so we only have to perform the install once
