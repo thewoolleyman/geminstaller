b9245c76e3735f6395f7895b5a0527c4e4bb0b36
Chad Woolley <thewoolleyman@gmail.com>
2007-03-06 06:40:28 +0000

    change windows comment
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@431 8563d94c-3d18-0410-b980-ae029421534c

--- spec/helper/embedded_gem_server.rb
+++ spec/helper/embedded_gem_server.rb
@@ -35,13 +35,11 @@ module GemInstaller
           print "NOTE: On windows, there is an orphaned gem_server ruby
       process left, which was a grandchild of the top-level ruby
       process which runs the tests, and a child of the cmd process
-      which runs gem_server.bat.  I can't figure out how to make
-      it die.  Luckily, there only seems to be one alive at any
-      given time, not an additional one for each test run, and this
-      one dies when the console used to run the tests is closed.\n\n"
-         print "WARNING: the windows tests are currently only passing
-      when run via 'rake test/test_all.rb.  They fail when run via
-      'rake test'\n"
+      which runs gem_server.bat.  Apparently, there's no way to kill
+      this, other than using the ruby-services package, which I haven't
+      done yet:
+      http://rubyforge.org/tracker/index.php?func=detail&aid=8957&group_id=85&atid=412
+      \n"
         end
         stopped = true
       end
