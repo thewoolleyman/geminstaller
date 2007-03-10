0772ec0068e51095b0297b7a37390977948b8724
Chad Woolley <thewoolleyman@gmail.com>
2007-03-10 21:25:26 +0000

    refactor and try to start unconvoluting noninteractive_chooser
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@440 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/noninteractive_chooser.rb
+++ lib/geminstaller/noninteractive_chooser.rb
@@ -3,9 +3,9 @@ module GemInstaller
   # Format for "uninstall" prompt list: "#{spec.name}-#{spec.version}" for ruby,  "#{spec.name}-#{spec.version}-#{spec.platform}" (gem.full_name) for binary
   class NoninteractiveChooser
     def specify_gem_spec(name, version, platform)
-      @required_name = name
-      @required_version = version
-      @required_platform = platform
+      @dependent_gem_name = name
+      @dependent_gem_version = version
+      @dependent_gem_platform = platform
     end
 
     def choose(question, list)
@@ -18,17 +18,17 @@ module GemInstaller
       required_version_regexp = ".*?"
       if uninstall_list_type? or dependent_gem?
          # match the exact name and version if it's an uninstall prompt or a dependent gem
-        required_name_regexp = Regexp.escape(@required_name) if @required_name
-        required_version_regexp = Regexp.escape(@required_version) if @required_version
+        required_name_regexp = Regexp.escape(@dependent_gem_name) if @dependent_gem_name
+        required_version_regexp = Regexp.escape(@dependent_gem_version) if @dependent_gem_version
       end
       required_list_item = "#{required_name_regexp}[\s-]{0,1}#{required_version_regexp}"
       # install list types always have the platform for each gem in parenthesis, even if it is ruby
-      if (install_list_type? && @required_platform)
-        required_list_item += Regexp.escape(" (#{@required_platform})")
+      if (install_list_type? && @dependent_gem_platform)
+        required_list_item += Regexp.escape(" (#{@dependent_gem_platform})")
       end
       # uninstall list types have the gem full_name, which is the platform for each gem appended after a dash, but only if it is not ruby
-      if (uninstall_list_type? && @required_platform && @required_platform.to_s != GemInstaller::RubyGem.default_platform)
-        required_list_item += Regexp.escape("-#{@required_platform}")
+      if (uninstall_list_type? && @dependent_gem_platform && @dependent_gem_platform.to_s != GemInstaller::RubyGem.default_platform)
+        required_list_item += Regexp.escape("-#{@dependent_gem_platform}")
       end
       required_list_item_regex = /^#{required_list_item}$/
       list.each_with_index do |item, index|
@@ -38,9 +38,9 @@ module GemInstaller
       end
       # if we didn't find a match, raise a descriptive error
       action = install_list_type? ? 'install' : 'uninstall'
-      name = @required_name ? "#{@required_name}" : "'any name'"
-      version = @required_version ? "#{@required_version}" : "'any version'"
-      platform = @required_platform ? " (#{@required_platform})" : ''
+      name = @dependent_gem_name ? "#{@dependent_gem_name}" : "'any name'"
+      version = @dependent_gem_version ? "#{@dependent_gem_version}" : "'any version'"
+      platform = @dependent_gem_platform ? " (#{@dependent_gem_platform})" : ''
       error_message = "Error: Unable to select gem from list: \"#{name} #{version}#{platform}\".  Available gems are:\n"
       list.each do |item|
         # skip last 'cancel' list entry, it's not a real gem
@@ -61,7 +61,7 @@ module GemInstaller
     def dependent_gem?
       # return true if it's an install prompt, and the list contains the gem for which the original install request
       # was made (in other words, it's a dependent gem, not a dependency gem)
-      install_format_exact_name_match_regexp = /^#{@required_name}\s.*/
+      install_format_exact_name_match_regexp = /^#{@dependent_gem_name}\s.*/
       install_list_type? and @list[0] =~ install_format_exact_name_match_regexp
     end
   
