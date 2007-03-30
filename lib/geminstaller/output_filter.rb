afb005629ac91ecf9c878ed04c2ebc26719b1a12
Chad Woolley <thewoolleyman@gmail.com>
2007-03-30 03:22:12 +0000

    r601@chadmac:  woolley | 2007-03-29 11:58:03 -0700  work on output filter
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@528 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/output_filter.rb
+++ lib/geminstaller/output_filter.rb
@@ -3,24 +3,35 @@ module GemInstaller
     attr_writer :output_proxy, :options
     
     def rubygems_output(type, message)
+      return unless rubygems_output_type_matches?(type)
       output(:rubygems, type, message)
     end
       
+    def geminstaller_output(type, message)
+      return unless geminstaller_output_type_matches?(type)
+      output(:geminstaller, type, message)
+    end
+      
     def output(source, type, message)
-      return unless type_matches?(type)
       formatted_message = format_message(source,message)
       @output_proxy.sysout(formatted_message)
     end
     
     def format_message(source, message)
       prefix = case source
-      when :rubygems : "[RubyGems]"
+      when :rubygems : "[RubyGems] "
       end
-      "#{prefix} #{message}"
+      "#{prefix}#{message}"
     end
 
-    def type_matches?(type)
+    def rubygems_output_type_matches?(type)
       return true if @options[:rubygems_output].include?(type)
+      return false
+    end
+
+    def geminstaller_output_type_matches?(type)
+      return true if @options[:geminstaller_output].include?(type)
+      return false
     end
   end
 end
--- spec/unit/output_filter_spec.rb
+++ spec/unit/output_filter_spec.rb
@@ -12,15 +12,22 @@ context "An output filter" do
     @expected_rubygems_message = "[RubyGems] #{@message}"
   end
 
-  specify "should pass rubygems stdout message to output proxy if stdout rubygems_output option is set" do
+  specify "should pass formatted rubygems stdout message to output proxy if stdout rubygems_output option is set" do
     @options[:rubygems_output] = [:stdout]
     @mock_output_proxy.should_receive(:sysout).with(@expected_rubygems_message)
     @output_filter.rubygems_output(:stdout,@message)
   end
 
-  specify "should pass not rubygems stdout message to output proxy if stdout rubygems_output option is not set" do
+  specify "should not pass rubygems stdout message to output proxy if stdout rubygems_output option is not set" do
     @options[:rubygems_output] = [:stderr]
     @output_filter.rubygems_output(:stdout,@message)
   end
+
+  specify "should pass geminstaller stdout message to output proxy if stdout geminstaller_output option is set" do
+    @options[:geminstaller_output] = [:stdout]
+    @mock_output_proxy.should_receive(:sysout).with(@message)
+    @output_filter.geminstaller_output(:stdout,@message)
+  end
+
 
 end
