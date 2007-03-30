63b90d8344c527ac18d9a82ed21225cf6f4f5064
Chad Woolley <thewoolleyman@gmail.com>
2007-03-30 03:54:22 +0000

    r612@chadmac:  woolley | 2007-03-29 20:50:32 -0700  try to make autogem work on rubygems 0.8
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@532 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/application.rb
+++ lib/geminstaller/application.rb
@@ -38,14 +38,21 @@ module GemInstaller
       completed_gems = []
       reversed_gems.each do |gem|
         unless completed_names.index(gem.name)
-          gem(gem.name, gem.version)
+          invoke_require_gem_command(gem.name, gem.version)
           completed_names << gem.name
           completed_gems << gem
         end
       end
       completed_gems
     end
-
+    
+    def invoke_require_gem_command(name, version)
+      if Gem::RubyGemsVersion.index('0.8') == 0
+        require_gem(name, version)
+      else
+        gem(name, version)
+      end
+    end
     
     def handle_args
       @arg_parser.parse(@args)
