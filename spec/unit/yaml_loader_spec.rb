f154e6f450d4e9d53e041b82b9339c2d67f07ad7
Chad Woolley <thewoolleyman@gmail.com>
2007-04-09 16:52:09 +0000

    r709@chadmac:  woolley | 2007-04-08 00:26:31 -0700  failing text for yaml quoting problem
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@580 8563d94c-3d18-0410-b980-ae029421534c

--- spec/unit/yaml_loader_spec.rb
+++ spec/unit/yaml_loader_spec.rb
@@ -5,6 +5,7 @@ context "YamlLoader instance" do
   setup do
     @yaml_loader = GemInstaller::YamlLoader.new
   end
+
   specify "should load yaml" do
     yaml = @yaml_loader.load('key: value')
     'value'.should==(yaml['key'])
@@ -19,5 +20,19 @@ context "YamlLoader instance" do
 
 
     proc_should_raise_with_message(/Error: Received error while attempting to parse YAML.*/) { @yaml_loader.load(@invalid_yaml_text) }
+  end
+
+  specify "should handle value of '> 1' even if it is not surrounded by quotes or followed by a newline" do
+    # TODO: make this pass, otherwise people must enclose version specs in quotes
+    
+    # key == '1'
+    yaml_text = "key: > 1\n"
+    # yaml parse error 
+    yaml_text = "key: > 1"
+    # key == '> 1'
+    yaml_text = "key: '> 1'"
+
+    yaml = @yaml_loader.load(yaml_text)
+    yaml['key'].should == '> 1'
   end
 end
