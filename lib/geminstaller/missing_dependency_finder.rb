6cb0dc3c0f4d220939a0720829e93d32dc3d94ab
Chad Woolley <thewoolleyman@gmail.com>
2007-04-15 07:52:32 +0000

    r797@chadmac:  woolley | 2007-04-15 00:49:33 -0700  refactor variable names
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@624 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/missing_dependency_finder.rb
+++ lib/geminstaller/missing_dependency_finder.rb
@@ -1,24 +1,24 @@
 module GemInstaller
   class MissingDependencyFinder
     attr_writer :gem_command_manager, :gem_arg_processor, :output_filter
-    def find(gem)
+    def find(dependent_gem)
       # NOTE: this doesn't resolve platforms, there's currently no way to know what
       # platform should be selected for a dependency gem.  Best-effort handling
       # of ambiguous platforms on dependency gems will be handled elsewhere
-      matching_gem_specs = @gem_command_manager.local_matching_gem_specs(gem)
+      matching_dependent_gem_specs = @gem_command_manager.local_matching_gem_specs(dependent_gem)
       missing_dependencies = []
-      install_options = gem.install_options
+      install_options = dependent_gem.install_options
       add_include_dependency_option(install_options)
       common_args = @gem_arg_processor.strip_non_common_gem_args(install_options)
-      matching_gem_specs.each do |matching_gem_spec|
+      matching_dependent_gem_specs.each do |matching_dependent_gem_spec|
         message_already_printed = false
-        dependency_gems = @gem_command_manager.dependency(matching_gem_spec.name, matching_gem_spec.version.to_s, common_args)
+        dependency_gems = @gem_command_manager.dependency(matching_dependent_gem_spec.name, matching_dependent_gem_spec.version.to_s, common_args)
         dependency_gems.each do |dependency_gem|
           dependency_gem.install_options = install_options
-          local_matching_gem_specs = @gem_command_manager.local_matching_gem_specs(dependency_gem)
-          unless local_matching_gem_specs.size > 0
+          local_matching_dependency_gem_specs = @gem_command_manager.local_matching_gem_specs(dependency_gem)
+          unless local_matching_dependency_gem_specs.size > 0
             unless message_already_printed
-              @output_filter.geminstaller_output(:info, "Missing dependencies found for #{matching_gem_spec.name} (#{matching_gem_spec.version}):\n")
+              @output_filter.geminstaller_output(:info, "Missing dependencies found for #{matching_dependent_gem_spec.name} (#{matching_dependent_gem_spec.version}):\n")
               message_already_printed = true
             end
             # TODO: print install options too?
