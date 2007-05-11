45e1c42f2e4369e87aa292d6e80a25e1084a62bb
Chad Woolley <thewoolleyman@gmail.com>
2007-05-11 04:40:51 +0000

    r1041@chadmac:  woolley | 2007-05-10 09:17:47 -0700  add comment
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@750 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller.rb
+++ lib/geminstaller.rb
@@ -65,7 +65,8 @@ module GemInstaller
     args_without_sudo.reject! {|arg| arg == "-s" || arg == "--sudo"}
     cmd = "sudo ruby #{geminstaller_executable} #{args_without_sudo.join(' ')}"
     # TODO: this eats any output.  There currently is no standard way to get a return code AND stdin AND stdout.
-    # Some non-standard packages like Open4 handle this.  
+    # Some non-standard packages like Open4 handle this, but not synchronously.  The Simplest Thing That Could
+    # Possibly Work s to have a command line option which will cause all stderr output to be redirected to stdout.
     result = system(cmd)
     return 1 unless result
     return $?.exitstatus
