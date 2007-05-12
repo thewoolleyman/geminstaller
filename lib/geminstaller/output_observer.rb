a29de275604066fa8c63a11638bc7f0aecd0cf95
Chad Woolley <thewoolleyman@gmail.com>
2007-05-12 08:18:58 +0000

    r1064@chadmac:  woolley | 2007-05-11 23:04:28 -0700  rdoc formatting
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@771 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/output_observer.rb
+++ lib/geminstaller/output_observer.rb
@@ -1,15 +1,7 @@
 module GemInstaller
   class OutputObserver
     attr_writer :stream
-    # TODO: 
-    # DONE Extract this to it's own file
-    # DONE: add a property for stream - :stdin/:stdout
-    # DONE: move OutputListener creation from GemRunnerProxy to be injected instead
-    # DONE: inject only a singleton instance of OutputListener
-    # DONE: inject output observer for both streams into enhanced_stream_ui, instead of creating them
-    # DONE: call notify on listener with :stdin/:stdout param
-    # DONE: inject the singleton listener into GemRunnerProxy
-    #  GemRunnerProxy should raise exception if listener still has any leftover output when run is invoked. 
+    # TODO: GemRunnerProxy should raise exception if listener still has any leftover output when run is invoked. 
     def initialize
       @stream = :stdout
       @listeners = []
