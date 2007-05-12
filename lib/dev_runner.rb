dc9e6f35a58ad4f28b64c90a4172aa96797a44ce
Chad Woolley <thewoolleyman@gmail.com>
2007-05-12 08:17:18 +0000

    r1054@chadmac:  woolley | 2007-05-10 23:26:15 -0700  remove dev_runner, no longer needed (just use bin/geminstaller)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@761 8563d94c-3d18-0410-b980-ae029421534c

--- lib/dev_runner.rb
+++
@@ -1,5 +0,0 @@
-dir = File.dirname(__FILE__)
-require File.expand_path("#{dir}/geminstaller.rb")
-
-# Run this file via Ruby from the "lib" dir to test the app without installing the gem
-GemInstaller.run
