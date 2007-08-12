366c098dd84ebcfa0ace312391274ccfcd308122
Chad Woolley <thewoolleyman@gmail.com>
2007-08-12 18:48:07 +0000

    r1198@chadmac:  woolley | 2007-08-12 08:03:56 -0700  remove extra const def
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@831 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/backward_compatibility.rb
+++ lib/geminstaller/backward_compatibility.rb
@@ -1,5 +1,4 @@
 # this file supports backward compatibility for prior versions of RubyGems
-RUBYGEMS_VERSION_CHECKER = GemInstaller::RubyGemsVersionChecker.new
 
 # 0.9.3 reorganized commands to Gem::Commands:: module from Gem::
 if RUBYGEMS_VERSION_CHECKER.less_than?('0.9.3')
