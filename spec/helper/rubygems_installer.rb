df60cf7cfc8412302fd4587c5fc5b8b43fa18d77
Chad Woolley <thewoolleyman@gmail.com>
2007-08-20 08:03:50 +0000

    r1207@chadmac:  woolley | 2007-08-19 19:02:15 -0700  ignore spec/tmp/test_gem.rc
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@835 8563d94c-3d18-0410-b980-ae029421534c

--- spec/helper/rubygems_installer.rb
+++ spec/helper/rubygems_installer.rb
@@ -4,15 +4,15 @@ module GemInstaller
     attr_writer :rubygems_dist_dir
     
     def install
+      puts "Installing RubyGems..."
       install_dir = File.expand_path(@install_dir)
       rubygems_dist_dir = File.expand_path(@rubygems_dist_dir)
       ENV['GEM_HOME'] = "#{install_dir}"
       setup_cmd = "ruby setup.rb"
       Dir.chdir("#{rubygems_dist_dir}") do
-        # TODO: use IO.popen to capture output, so it doesn't spam the tests
-        `#{setup_cmd} config --prefix=#{install_dir}`
-        `#{setup_cmd} setup`
-        `#{setup_cmd} install`
+        `#{setup_cmd} --quiet config --prefix=#{install_dir}`
+        `#{setup_cmd} --quiet setup`
+        `#{setup_cmd} --quiet install`
       end
     end
   end
