2691d7acfd27cd5e1610b8e167619182ea17d590
Chad Woolley <thewoolleyman@gmail.com>
2007-12-29 09:43:56 +0000

    add comment about scenario where rubygems version is incorrect
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@923 8563d94c-3d18-0410-b980-ae029421534c

--- lib/geminstaller/rubygems_version_checker.rb
+++ lib/geminstaller/rubygems_version_checker.rb
@@ -4,6 +4,10 @@ module GemInstaller
   class RubyGemsVersionChecker
     def matches?(version_spec, options = {})
       version_spec = [version_spec] unless version_spec.kind_of?(Array)
+      # TODO: if rubygems has already been initialized before GemInstaller overrides the rubygems version,
+      # (for example, by running rspec), then Gem::RubyGemsVersion could be initialized to the incorrect
+      # system-rubygems version instead of the geminstaller-overridden version.  Need to figure out how
+      # to re-parse 'rubygems/rubygems_version' and let it redefine 'Gem::RubyGemsVersion'
       rubygems_version = options[:rubygems_version] ||= Gem::RubyGemsVersion
       Gem::Version::Requirement.new(version_spec).satisfied_by?(Gem::Version.new(rubygems_version))
     end
