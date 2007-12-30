698c088ae04a6caebea509767e2748771eb82240
Chad Woolley <thewoolleyman@gmail.com>
2007-12-30 08:55:51 +0000

    fix return code from test_all
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@935 8563d94c-3d18-0410-b980-ae029421534c

--- test/test_all.rb
+++ test/test_all.rb
@@ -3,8 +3,9 @@ specdir = File.expand_path("#{dir}/../spec")
 require File.expand_path("#{specdir}/helper/spec_helper")
 
 spec_files = Dir.glob("#{specdir}/**/*_spec.rb")
+
 # put test_gem_home_spec first so we only have to perform the install once
-spec_files.unshift(Dir.glob("#{specdir}/unit/test_gem_home_spec.rb")[0])
+spec_files.unshift(Dir.glob("#{specdir}/functional/test_gem_home_spec.rb")[0])
 spec_files.uniq
 
 args = spec_files
@@ -18,12 +19,14 @@ args << "unified"
 # but I don't know a better way to invoke Rspec 1.1.1 programatically and check the return value
 ARGV.concat(args)
 
-retval = 1
+success = false
 begin
   require 'spec'
-  retval = ::Spec::Runner::CommandLine.run(rspec_options)
+  success = ::Spec::Runner::CommandLine.run(rspec_options)
 ensure
   GemInstaller::TestGemHome.reset
 end
-retval ||= 0
-raise "Specs failed, return value = #{retval}" unless (retval == 0 || retval == true)
+# not sure what Spec::Runner::CommandLine.run can return, but fail if it's not boolean as expected
+raise "unexpected non-boolean return value from Spec::Runner::CommandLine.run(rspec_options)" unless success == true || success == false
+
+raise "Specs failed" unless success
