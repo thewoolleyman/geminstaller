2626ebe17845d57040990a56b9f1bd88f856b91b
Chad Woolley <thewoolleyman@gmail.com>
2008-01-02 03:01:55 +0000

    updates to prepare for 0.3.0 release
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@986 8563d94c-3d18-0410-b980-ae029421534c

--- focused_spec.sh
+++ focused_spec.sh
@@ -1,1 +1,2 @@
-ruby -I spec/fixture/rubygems_dist/rubygems-1.0.0/lib/ /usr/local/bin/spec $1 --line=$2
+# warning - experimental
+ruby -I spec/fixture/rubygems_dist/rubygems-1.0.1/lib/ /usr/local/bin/spec $1 --line=$2
--- focused_spec_debug.sh
+++ focused_spec_debug.sh
@@ -1,1 +1,2 @@
+# warning - experimental
 rdebug -I spec/fixture/rubygems_dist/rubygems-1.0.0/lib/ /usr/local/bin/spec -- $1 --line=$2
