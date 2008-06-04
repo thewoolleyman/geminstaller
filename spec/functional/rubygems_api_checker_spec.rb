fbf6c03695847178459db82cb9cbc6c9382ea8b7
Chad Woolley <thewoolleyman@gmail.com>
2008-06-04 02:49:28 +0000

    use open4 to manage embedded gem server on non-windows, add rubygems_api_checker_spec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@1083 8563d94c-3d18-0410-b980-ae029421534c

---
+++ spec/functional/rubygems_api_checker_spec.rb
@@ -0,0 +1,26 @@
+dir = File.dirname(__FILE__)
+require File.expand_path("#{dir}/../helper/spec_helper")
+
+# This spec is intended to identify breakages caused by changes in the RubyGems API, 
+# specifically ones which cause the TestGemHome and EmbeddedGemServer to break,
+# which in turn cause all functional tests to break.  This is also an easy place to debug
+# said breakages...
+describe "RubyGems API" do
+  before(:each) do
+    GemInstaller::TestGemHome.use
+    @registry = GemInstaller::create_registry
+    @gem_runner_proxy = @registry.gem_runner_proxy
+  end
+
+  if GemInstaller::RubyGemsVersionChecker.matches?('>=1.1.1')
+  # we only care about latest version of RubyGems
+  it "should return the expected fixture environment" do
+    gem_runner_args = ["env"]
+
+    output = @gem_runner_proxy.run(gem_runner_args)
+    #puts output.join("\n")
+    output.join.should match(/GEM PATHS.*test_gem_home/m)
+    
+  end
+  end
+end
--- spec/helper/spec_helper.rb
+++ spec/helper/spec_helper.rb
@@ -18,6 +18,8 @@ require File.expand_path("#{dir}/embedded_gem_server.rb")
 if RUBY_PLATFORM.index('mswin')
   require 'win32/process'
   require 'win32/open3'
+else
+  require 'open4'
 end
 # ruby-debug indirectly requires 'irb', which makes require of 'rdoc/rdoc' in rubygems doc_manager.rb blow up
 #require 'ruby-debug'
