d4921641aa54577c779f1e6cab0f50195be18241
Chad Woolley <thewoolleyman@gmail.com>
2008-06-10 19:53:06 +0000

    add manual tests to reproduce rubygems bugs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@1088 8563d94c-3d18-0410-b980-ae029421534c

---
+++ spec/manualtest/gem_list.sh
@@ -0,0 +1,1 @@
+ruby -I ../fixture/rubygems_dist/rubygems-9.9.9/lib:../fixture/rubygems_dist/rubygems-9.9.9/bin ../fixture/rubygems_dist/rubygems-9.9.9/bin/gem list --remote --all --source http://127.0.0.1:8808
---
+++ spec/manualtest/start_gem_server.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.gem && ruby -I ../fixture/rubygems_dist/rubygems-9.9.9/lib:../fixture/rubygems_dist/rubygems-9.9.9/bin ../fixture/rubygems_dist/rubygems-9.9.9/bin/gem server -d ../fixture/gems --debug
+
