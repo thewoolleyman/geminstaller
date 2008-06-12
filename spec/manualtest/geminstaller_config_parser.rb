813e47d3353406cf6ad03e50b84648e217d63b8d
Chad Woolley <thewoolleyman@gmail.com>
2008-06-12 15:29:39 +0000

    example of manually parsing geminstaller configs, inspired by John Trupiano
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@1092 8563d94c-3d18-0410-b980-ae029421534c

---
+++ spec/manualtest/geminstaller_config_parser.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'geminstaller'
+
+config_file_paths = ARGV[0]
+raise "Usage: geminstaller_config_parser <paths_to_geminstaller_config_files>" unless config_file_paths
+
+registry = GemInstaller::Registry.new
+config_builder = registry.config_builder
+config_builder.config_file_paths = config_file_paths
+config = config_builder.build_config
+
+require 'pp'
+pp config
