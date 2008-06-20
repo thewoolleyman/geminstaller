378d7e46bb272df827cb05ea62cdaf99386177ef
Chad Woolley <thewoolleyman@gmail.com>
2008-06-20 04:00:00 +0000

    another manual test script
    git-svn-id: svn+ssh://rubyforge.org/var/svn/geminstaller/trunk@1099 8563d94c-3d18-0410-b980-ae029421534c

---
+++ spec/manualtest/gem_install_multilevel.sh
@@ -0,0 +1,1 @@
+ruby -I ../fixture/rubygems_dist/rubygems-9.9.9/lib:../fixture/rubygems_dist/rubygems-9.9.9/bin ../fixture/rubygems_dist/rubygems-9.9.9/bin/gem install dependent-stubgem-multilevel --version=1.0.0 --remote --source=http://127.0.0.1:8808
