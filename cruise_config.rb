5adb41e1393d68becbcb7e4f94b3900f3a777d64
Chad Woolley <thewoolleyman@gmail.com>
2009-07-17 03:25:46 -0700

    testing cruise triggers

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,8 +2,12 @@
 require 'fileutils'
 
 Project.configure do |project|
-  project.triggered_by :dummyrepo
-  project.triggered_by :RubyGems
+  if File.exists?(File.expand_path(File.dirname(project.path)) + '/../../RubyGems')
+    project.triggered_by :RubyGems
+  end
+  if File.exists?(File.expand_path(File.dirname(project.path)) + '/../../dummyrepo')
+    project.triggered_by :dummyrepo
+  end
   project.email_notifier.emails = ['thewoolleyman@gmail.com']
   if project.name =~ /rubygems[_-](.*)$/ # geminstaller_using_rubygems_0-9-4
     rubygems_version = $1
